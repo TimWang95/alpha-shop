6c7870bcefc9b02cb2d10c07d1c1b5fcfd80a93e
TimWang95 <tgm761@gmail.com>
2023-04-13 14:26:35 +0800

    feat: add  handleQuantityClick

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,27 +1,51 @@
 import { ReactComponent as Minus } from "../../assets/icons/minus.svg"
 import { ReactComponent as Plus } from "../../assets/icons/plus.svg"
 import cartData from "../../context/CartContext"
+import { useState } from "react"
 
-function CartData({ id, name, img, price, quantity }) {
+
+function CartData() {
+  const [ products, setProducts ] = useState(cartData)
+
+  function handleQuantityClick(productId, action) {
+    const nextProducts = products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...products,
+          quantity: action === 'minus' ? product.quantity - 1 : product.quantity + 1
+        }
+      } else {
+        console.log(product.id)    
+        return product
+      }
+    })
+    const updataProducts = nextProducts.filter((product) => product.quantity > 0)
+    setProducts(updataProducts)
+  }
+
   return (
     <>
-      {cartData.map(data =>
+      {products.map(product =>
         <div className="product-container col col-12" 
-        id={data.id}
-        key={data.id}
-        data-count={data.quantity} 
-        data-price={data.price}>
-          <img className="img-container" src={data.img} />
+        id={product.id}
+        key={product.id}
+        data-count={product.quantity} 
+        data-price={product.price}>
+          <img className="img-container" src={product.img} alt={product.name}/>
           <div className="product-info">
-            <div className="product-name">{data.name}</div>
+            <div className="product-name">{product.name}</div>
             <div className="product-control-container">
               <div className="product-control">
-                <Minus className="product-action minus" />
-                <span className="product-count">{data.quantity}</span>
-                <Plus className="product-action plus"/>
+                <Minus className="product-action minus" onClick={() => {
+                  handleQuantityClick(product.id, 'minus')
+                }} />
+                <span className="product-count">{product.quantity}</span>
+                <Plus className="product-action plus" onClick={() => {
+                  handleQuantityClick(product.id, 'plus')
+                }} />
               </div>
             </div>
-            <div className="price">${data.price}</div>
+            <div className="price">${product.price}</div>
           </div>
         </div>
       )}
